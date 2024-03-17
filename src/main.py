94bc6d0dc0cdaf5e032639340f7b0a586b23e29f
WolfPack9108 <89105476+Flow-Glow@users.noreply.github.com>
2024-03-17 00:22:17 -0700

    base with inf falling

--- src/main.py
+++ src/main.py
@@ -1,23 +1,25 @@
 import pyxel
 
+from .player import Player
+
 
 class App:
     """Main application class."""
 
-    FPS = 10
+    FPS = 60
     TITLE = "Pyweek37"
 
     def __init__(self) -> None:
         pyxel.init(160, 120, title=self.TITLE, fps=self.FPS)
-
+        self.player = Player()
         pyxel.run(self.update, self.draw)
-
     def update(self) -> None:
         """
         Update the game state.
 
         :return:
         """
+        self.player.update()
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
 
@@ -28,3 +30,4 @@ class App:
         :return:
         """
         pyxel.cls(0)
+        self.player.draw()
