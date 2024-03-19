918f7a443aa8304688e56694cec4f7cc811631b6
mit-mit <randomprojectlab@gmail.com>
2024-03-19 11:28:16 +1100

    add sfx module, added get_tile_at_xy method in map

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import os
 from .player import Player
 from .map import Map
 from .input import Input
+from .sfx import Sfx
 
 class App:
     """Main application class."""
@@ -14,9 +15,12 @@ class App:
     def __init__(self) -> None:
         pyxel.init(120, 160, title=self.TITLE, fps=self.FPS)
         pyxel.load("../Assets/tube.pyxres")
+        pyxel.load("../Assets/tube_audio.pyxres", True, True, False, False) # just loads audio
+        pyxel.playm(0, 0, True) # start main music
         self.input = Input()
         self.player = Player(self.input)
         self.map = Map()
+        self.sfx = Sfx()
         self.playing = False
         pyxel.run(self.update, self.draw)
 
@@ -29,6 +33,8 @@ class App:
         if self.playing:
             self.player.update()
             self.map.update(self.player.scroll_y)
+            (tilex,tiley) = self.map.get_tile_at_xy(self.player.x+8, self.player.y)
+            self.sfx.update_ground_sound(tilex, tiley)
         elif self.input.update():
             self.playing = True
 
